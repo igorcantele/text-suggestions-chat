a5628a8c016126875d583de0efb235f4b07275c4
Igor10798 <igor.cantele01@universitadipavia.it>
2023-09-20 20:41:54 +0200

    Added function to insert element in cache

--- src/suggestions/suggestions.ts
+++ src/suggestions/suggestions.ts
@@ -1,4 +1,5 @@
 import { Suggestion } from './entities/suggestion.entity';
+import { binarySearch } from '../helpers';
 
 export const UPDATE_CHAIN_QUEUE = 'update-chain-queue';
 
@@ -8,12 +9,23 @@ export const insertInCacheFn = (
   prevVals: SuggestionNodeElem[],
   newVal: SuggestionNodeElem,
 ): [SuggestionNodeElem[], number] => {
-  const foundElem = prevVals.find((node) => (node.sugg = newVal.sugg));
-  const increment: number = foundElem ? 0 : 1;
+  const foundIdx = prevVals.findIndex((node) => (node.sugg = newVal.sugg));
+  const increment: number = foundIdx !== -1 ? 0 : 1;
 
-  if (foundElem) {
-    foundElem.freq++;
+  if (foundIdx !== -1) {
+    prevVals[foundIdx].freq++;
+
+    // Setting a pointer to find the most left element with freq < than the current increased freq
+    let curr = foundIdx;
+    while (prevVals[curr - 1].freq < prevVals[curr].freq) {
+      curr--;
+    }
+    // Switching elements in the array
+    const tmp = prevVals[curr + 1];
+    prevVals[curr + 1] = prevVals[foundIdx];
+    prevVals[foundIdx] = tmp;
   } else {
+    // If the element has freq 1 can be added at the end of the array
     prevVals.push(newVal);
   }
 
